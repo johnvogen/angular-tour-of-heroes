da5dde38731ae8c4336cdbb6b03c085f582f8326
John Vogen <john.vogen@verizon.net>
2018-04-29 22:52:08 -0400

    following the tutorial this is now up to date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour Of Heros';
-  subTitle = "Johns First App"
+  title = 'Tour of Heroes';
 }
